960983b103122ca1fb7a223a99186811371070b5
chinmayakumardas <chinmayakumardas2000@gmail.com>
2025-07-17 19:12:01 +0530

    dwfvr

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+// next.config.mjs
+const nextConfig = {
+  basePath: '/note',
+  assetPrefix: '/note',
+};
 
 export default nextConfig;
